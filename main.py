344f25cf4f9925ef610d8d2e96b24339eba35c04
maharengarajan <maharengarajan46@gmail.com>
2025-07-14 12:52:51 +0530

    main pipeline update

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from src.datascience import LoggerConfig
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
 
 logger = LoggerConfig.get_logger()
 
-logger.info("Service started")
-logger.error("Something went wrong", exc_info=True)
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
